19f6b1ba5e84691726a05db04e4a3b561eea42c3
przemyslawzaworski <przemyslawzaworski@gmail.com>
2017-05-07 16:38:52 +0200

    Small utility for release builds.

---
+++ quit_and_cursoroff.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class quit_and_cursoroff : MonoBehaviour 
+{
+	void Start ()
+	{
+		Cursor.visible = false;
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown("escape")) Application.Quit();
+	}
+}
