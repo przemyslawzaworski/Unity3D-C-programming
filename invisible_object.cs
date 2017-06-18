9a77619e9a1e3c96be4ca72f4afae3bc6d639895
przemyslawzaworski <przemyslawzaworski@gmail.com>
2017-06-18 18:30:29 +0200

    Disable object rendering.

---
+++ invisible_object.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class invisible_object : MonoBehaviour 
+{
+	void Start () 
+	{
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.enabled = false;
+	}	
+}
