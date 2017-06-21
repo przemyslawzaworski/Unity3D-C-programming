3396b01aff79cded9cbaed04257bd377045c404a
przemyslawzaworski <przemyslawzaworski@gmail.com>
2017-06-21 15:04:58 +0200

    Script writes sample key into Windows Registry.

---
+++ register.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Microsoft.Win32;
+
+public class register : MonoBehaviour 
+{
+	void Start () 
+	{
+		RegistryKey handle = Registry.CurrentUser;
+		handle = handle.OpenSubKey("Software", true);
+		RegistryKey key = handle.CreateSubKey("Game");
+		key.SetValue("Fullscreen",1);
+		handle.Close();
+	}
+}
