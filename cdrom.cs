e9807b70df255cde24578cbbfb6ae56252005bfe
przemyslawzaworski <przemyslawzaworski@gmail.com>
2017-09-28 23:58:05 +0200

    Insert and eject CDROM device.

---
+++ cdrom.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.InteropServices;
+ 
+public class cdrom : MonoBehaviour 
+{
+	[DllImport("winmm.dll", EntryPoint = "mciSendStringA", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)] 
+	private static extern long mciSendString(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+	
+	void Start () 
+	{
+		mciSendString ("Set cdaudio door open wait",null,0,0);
+		mciSendString ("Set cdaudio door closed wait",null,0,0);
+	}
+}
