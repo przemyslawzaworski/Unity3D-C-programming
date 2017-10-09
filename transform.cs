945b403809cbee6cf00091bc87111426343bc2e0
przemyslawzaworski <przemyslawzaworski@gmail.com>
2017-10-09 12:18:48 +0200

    Translation, rotation, scale.

---
+++ transform.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class transform : MonoBehaviour 
+{
+	Vector3 StartPosition;
+	Vector3 StartScale;
+	void Awake()
+	{
+		StartPosition = transform.position;
+		StartScale = new Vector3(1,1,1);
+	}
+	
+	void Update() 
+	{
+		if (Input.GetKey("a")) transform.position = new Vector3(0, 0, 0); 
+		else 
+		if (Input.GetKey("b")) transform.Translate(new Vector3(0,0.1f,0));
+		else
+		if (Input.GetKey("c")) transform.Rotate(new Vector3(0,10,0));	
+		else
+		if (Input.GetKey("d")) transform.localScale+=(new Vector3(0.1f,0.1f,0.1f) );		
+		else 
+		{
+			transform.position=StartPosition;
+			transform.localScale=StartScale;
+		}
+    }
+}
