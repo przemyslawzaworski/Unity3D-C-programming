9ae1e7a5b26a5894c7917b18334cc5d7bc94d5a8
przemyslawzaworski <przemyslawzaworski@gmail.com>
2017-10-17 08:47:02 +0200

    Fibonacci sequence - iterative method

---
+++ fibonacci.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fibonacci : MonoBehaviour 
+{
+    public static int Fibonacci(int n)
+    {
+		int a = 0;
+		int b = 1;
+		int c = 0;
+		for (int i = 0; i < n; i++)
+		{
+			c = a;
+			a = b;
+			b = c + b;
+		}
+		return a;
+	}
+
+	void Start () 
+	{
+		int total = 0;
+		for (int k=0;k<32;k++)
+		{
+			total=total+Fibonacci(k);
+		}
+		Debug.Log("Sum of the first 32 elements of Fibonacci sequence= "+total);
+	}
+	
+}
