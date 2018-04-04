c482de3e646b2b266fd1dddb9304b309077b1e27
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2018-04-04 11:47:16 +0200

    Basic wireframe rendering.

---
+++ wireframe.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wireframe : MonoBehaviour 
+{
+	public bool enable;
+	public Camera main_camera;
+
+	void OnPreRender() 
+	{
+		if (enable) 
+		{
+			main_camera.clearFlags = CameraClearFlags.SolidColor;
+			GL.wireframe = true;
+		}
+		else
+		{
+			GL.wireframe = false;
+			main_camera.clearFlags = CameraClearFlags.Skybox;
+		}
+	}
+
+	void OnPostRender() 
+	{
+		GL.wireframe = false;
+	}
+}
