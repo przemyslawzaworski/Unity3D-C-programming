e8d3dd717f8b922b3e596475e315d74a1c5fe40f
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2018-04-06 17:36:17 +0200

    Basic setup for scripting Post Processing Stack.

---
+++ post_processing_stack_setup.cs
@@ -0,0 +1,21 @@
+﻿//Player Settings should has Scripting Define Symbol: UNITY_POST_PROCESSING_STACK_V1
+
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class post_processing_stack_setup : MonoBehaviour 
+{
+	public PostProcessingProfile PPP;
+	
+	void Start ()
+	{
+		PPP.colorGrading.enabled = true;		
+	}
+	
+	void Update () 
+	{
+		ColorGradingModel.Settings cgms = PPP.colorGrading.settings;
+		cgms.basic.saturation = Mathf.Lerp(0.0f,1.0f,Mathf.Sin(Time.time)*0.5f+0.5f);
+		PPP.colorGrading.settings = cgms;		
+	}
+}
