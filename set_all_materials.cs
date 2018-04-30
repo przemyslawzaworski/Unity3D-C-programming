bfa9cf76444a66ddfac4eb70fc0bcc2d303303fd
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2018-04-30 13:25:01 +0200

    Add files via upload

---
+++ set_all_materials.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class set_all_materials : MonoBehaviour 
+{
+	public GameObject target;
+	public Material material;
+
+	void SetAllMaterials(GameObject game_object, Material new_material)
+	{
+		Renderer[] children;
+		children = game_object.GetComponentsInChildren<Renderer>();
+		foreach (Renderer renderer in children)
+		{
+			Material[] materials = new Material[renderer.materials.Length];
+			for (int i = 0; i < renderer.materials.Length; i++) materials[i] = new_material;
+			renderer.materials = materials;
+		}
+	}
+
+	void Start () 
+	{
+		SetAllMaterials(target,material);
+	}
+
+}
