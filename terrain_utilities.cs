933b94ff04316c6bc4e37c0a891f58f50c51c0fd
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2018-06-12 21:04:43 +0200

    Terrain utilities

---
+++ terrain_utilities.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class terrain_utilities : MonoBehaviour 
+{
+	public Terrain terrain;
+
+	float GetTerrainMaximumHeightLocalSpace (Terrain terrain)
+	{
+		return terrain.terrainData.bounds.max.y;
+	}
+
+	float GetTerrainMaximumHeightWorldSpace (Terrain terrain)	
+	{
+		float a = terrain.terrainData.bounds.max.y;
+		float b = terrain.transform.position.y;	
+		return a+b;
+	}
+	
+	float GetTerrainMaximumHeightNormalized (Terrain terrain)
+	{
+		float a = terrain.terrainData.bounds.max.y;
+		float b = terrain.terrainData.size.y;	
+		return a/b;
+	}
+	
+}
