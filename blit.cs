9bb86b33e0a47566def5580e67012bbf1e607e3b
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2018-06-17 23:51:19 +0200

    Copy render textures.

---
+++ blit.cs
@@ -0,0 +1,60 @@
+﻿//copy source render texture to destination, with material
+
+using UnityEngine;
+
+public class blit : MonoBehaviour 
+{
+	public RenderTexture Source;
+	public RenderTexture Destination;
+	public Material ExternalMaterial;
+	
+	protected Material InternalMaterial;
+		
+	void Blit(RenderTexture source, RenderTexture destination)
+	{
+		if (!InternalMaterial) InternalMaterial = new Material(Shader.Find("Sprites/Default"));
+		RenderTexture.active = destination;
+		InternalMaterial.SetTexture("_MainTex", source);
+		GL.PushMatrix();
+		GL.LoadOrtho();
+		InternalMaterial.SetPass(0);
+		GL.Begin(GL.QUADS);
+		GL.MultiTexCoord2(0, 0.0f, 0.0f);
+		GL.Vertex3(0.0f, 0.0f, 0.0f);
+		GL.MultiTexCoord2(0, 1.0f, 0.0f);
+		GL.Vertex3(1.0f, 0.0f, 0.0f); 
+		GL.MultiTexCoord2(0, 1.0f, 1.0f);
+		GL.Vertex3(1.0f, 1.0f, 1.0f); 
+		GL.MultiTexCoord2(0, 0.0f, 1.0f);
+		GL.Vertex3(0.0f, 1.0f, 0.0f);
+		GL.End();
+		GL.PopMatrix();		
+	}
+	
+	void Blit(RenderTexture source, RenderTexture destination, Material mat)
+	{
+		RenderTexture.active = destination;
+		mat.SetTexture("_MainTex", source);
+		GL.PushMatrix();
+		GL.LoadOrtho();
+		GL.invertCulling = true;
+		mat.SetPass(0);
+		GL.Begin(GL.QUADS);
+		GL.MultiTexCoord2(0, 0.0f, 0.0f);
+		GL.Vertex3(0.0f, 0.0f, 0.0f);
+		GL.MultiTexCoord2(0, 1.0f, 0.0f);
+		GL.Vertex3(1.0f, 0.0f, 0.0f); 
+		GL.MultiTexCoord2(0, 1.0f, 1.0f);
+		GL.Vertex3(1.0f, 1.0f, 1.0f); 
+		GL.MultiTexCoord2(0, 0.0f, 1.0f);
+		GL.Vertex3(0.0f, 1.0f, 0.0f);
+		GL.End();
+		GL.invertCulling = false;
+		GL.PopMatrix();		
+	}
+	
+	void Update () 
+	{
+		Blit(Source,Destination,ExternalMaterial);
+	}
+}
