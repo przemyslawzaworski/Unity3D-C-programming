bb2c409f22f8d775bb5d8a3d28f49777224421ad
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2018-06-17 14:26:44 +0200

    Simple button script

---
+++ button.cs
@@ -0,0 +1,21 @@
+﻿// UI -> Button
+// Execute a function after button click, then show button image name
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class button : MonoBehaviour 
+{
+	public Button DebugButton;
+	
+	void Start () 
+	{
+		DebugButton.onClick.AddListener(ShowCaption);
+	}
+	
+	void ShowCaption () 
+	{
+		string ImageName = EventSystem.current.currentSelectedGameObject.GetComponent<Image>().sprite.name;
+		Debug.Log(ImageName);
+	}
+}
