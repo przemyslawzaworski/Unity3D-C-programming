4ba27b54c8474892d539ff769fe040fb04d542c5
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2018-07-12 21:28:20 +0200

    Simple tool.

---
+++ text_to_quotes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class text_to_quotes : MonoBehaviour 
+{
+	public string InputPath;
+	public string OutputPath;
+	
+	void Start () 
+	{
+		StreamReader reader = new StreamReader(InputPath);
+		StreamWriter writer = new StreamWriter(OutputPath);
+		while (!reader.EndOfStream)
+		{
+			string line = "\"" + reader.ReadLine() + "\"";
+			writer.WriteLine(line);		
+		}
+		reader.Close();
+		writer.Close();
+		Debug.Log("Done.");
+	}
+	
+}
