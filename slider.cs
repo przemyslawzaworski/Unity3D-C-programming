867fb0f8d40d9694f220b909380f0a30db5dba12
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2018-10-08 20:56:41 +0200

    UI slider

---
+++ slider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class slider : MonoBehaviour 
+{
+	public GameObject Target;
+	public Slider MainSlider;
+	
+	void Start () 
+	{
+		MainSlider.onValueChanged.AddListener(delegate {Shift();} );		
+	}
+	
+	void Shift ()
+	{
+		Target.transform.position = new Vector3(MainSlider.value, 0, 0);
+	}
+}
