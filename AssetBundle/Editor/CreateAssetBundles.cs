57b52230fe31b9a1b4836b26d3c9c6083134448d
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2019-07-02 10:42:45 +0200

    Create and load data from AssetBundle

---
+++ AssetBundle/Editor/CreateAssetBundles.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles : EditorWindow
+{
+	string AssetBundleName = "";
+	string assetBundleDirectory = "Assets/StreamingAssets";
+	
+	[MenuItem("Assets/CreateAssetBundles")]
+	static void ShowWindow () 
+	{
+		EditorWindow.GetWindow ( typeof(CreateAssetBundles));
+	}
+
+	void OnGUI()
+	{
+		AssetBundleName = EditorGUILayout.TextField("Bundle name: ", AssetBundleName);
+		if ( GUILayout.Button( "Generate" ) ) BuildAllAssetBundles();
+	} 
+		
+	void BuildAllAssetBundles()
+	{
+		string[] AssetNames = new string[Selection.transforms.Length];
+		
+		for (int i=0; i<AssetNames.Length; i++)
+		{
+			GameObject prefab = PrefabUtility.GetOutermostPrefabInstanceRoot(Selection.transforms[i].gameObject);
+			UnityEngine.Object source = null;
+			if (prefab != null)
+			{ 
+				source = PrefabUtility.GetCorrespondingObjectFromSource(prefab); 
+			}
+			AssetNames[i] = AssetDatabase.GetAssetPath(source);
+		}
+		
+		AssetBundleBuild[] bundles = new AssetBundleBuild[]
+		{
+			new AssetBundleBuild()
+			{
+				assetBundleName = AssetBundleName,
+				assetNames = AssetNames
+			}
+		};
+
+		if(!Directory.Exists(assetBundleDirectory))
+		{
+			Directory.CreateDirectory(assetBundleDirectory);
+		}
+		BuildPipeline.BuildAssetBundles(assetBundleDirectory,bundles,BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+	}
+}
---
+++ AssetBundle/LoadAssetBundle.cs
@@ -0,0 +1,25 @@
+﻿// Example script
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAssetBundle : MonoBehaviour 
+{
+	public string AssetBundlePackName;
+	public string[] PrefabNames;
+	
+	void Start () 
+	{
+		var Archive = AssetBundle.LoadFromFile(Application.streamingAssetsPath+"/"+AssetBundlePackName);
+		if (Archive == null) 
+		{
+			Debug.Log("Failed to load AssetBundle!");
+			return;
+		}
+		for (int i=0; i<PrefabNames.Length; i++)
+		{
+			var source = Archive.LoadAsset<GameObject>(PrefabNames[i]);
+			Instantiate(source);
+		}		
+	}
+}
