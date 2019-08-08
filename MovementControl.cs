4758286760ecd3b5d18299318b7a9ff892392af7
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2019-08-08 23:39:52 +0200

    Whether object moving towards/backwards/static.

---
+++ MovementControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MovementControl : MonoBehaviour
+{
+	Vector3 direction = Vector3.zero;
+	Vector3 previous = Vector3.zero;
+	Vector3 current = Vector3.zero;
+	Vector3 forward = Vector3.zero;
+ 
+	void Update()
+	{
+		current = transform.position;
+		direction = (current - previous);
+		if(Vector3.Dot(forward, direction) < 0)
+			Debug.Log("Moving backwards");
+		else if(Vector3.Dot(forward, direction) > 0)
+			Debug.Log("Moving forwards"); 
+		else Debug.Log("Static");
+	}
+
+	void LateUpdate()
+	{
+		previous = transform.position;
+		forward = transform.forward;
+	}
+}
