e90a39c37d6437442597c538e3785556bdb38481
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2019-10-22 10:45:21 +0200

    Dump systeminfo

---
+++ error.cs
@@ -0,0 +1,36 @@
+﻿// Dump systeminfo to output.log (build) or console
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+public class error : MonoBehaviour
+{
+	void OnEnable()
+	{
+		Application.logMessageReceived += LogCallback;
+	}
+
+	void LogCallback(string condition, string stackTrace, LogType type)
+	{
+		string result = "";
+		foreach(var property in typeof(SystemInfo).GetProperties()) 
+		{
+			if (property.CanRead) result = result + property.Name + " = " + property.GetValue(null).ToString() + " \n";
+		}
+		Debug.Log(result);
+	}
+
+	void OnDisable()
+	{
+		Application.logMessageReceived -= LogCallback;
+	}
+	
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			float num = System.Single.Parse("abc");   //simulates error
+		}
+	}
+}
