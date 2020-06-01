a601e0e7d95d11ff01f75c51f4f99853cd6f8e9d
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2020-06-02 01:00:39 +0200

    Load DDS textures

---
+++ LoadDDS.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.IO;
+using System;
+
+public static class LoadDDS
+{
+	public static Texture2D Load(string path)
+	{
+		try
+		{
+			BinaryReader reader = new BinaryReader(File.OpenRead(path));
+			long length = new FileInfo(path).Length;
+			byte[] header = reader.ReadBytes(128);
+			int height = header[13] * 256 + header[12];
+			int width = header[17] * 256 + header[16];
+			bool mipmaps = header[28] > 0;
+			TextureFormat textureFormat = header[87] == 49 ? TextureFormat.DXT1 : TextureFormat.DXT5;
+			byte[] source = reader.ReadBytes(Convert.ToInt32(length) - 128);
+			reader.Close();
+			Texture2D texture = new Texture2D(width, height, textureFormat, mipmaps);
+			texture.LoadRawTextureData(source);
+			texture.name = Path.GetFileName(path);
+			texture.Apply(false, true);
+			return texture;
+		}
+		catch (Exception)
+		{
+			return Texture2D.blackTexture;
+		}
+	}
+}
---
+++ LoadTextureAsync.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using System.Runtime.CompilerServices;
+
+public class UnityWebRequestAwaiter : INotifyCompletion
+{
+	UnityWebRequestAsyncOperation request;
+	Action action; 
+
+	public UnityWebRequestAwaiter(UnityWebRequestAsyncOperation request)
+	{
+		this.request = request;
+		request.completed += OnRequestCompleted;
+	}
+
+	public bool IsCompleted { get { return request.isDone; } }
+
+	public void GetResult() { }
+
+	public void OnCompleted(Action action)
+	{
+		this.action = action;
+	}
+
+	private void OnRequestCompleted(AsyncOperation command)
+	{
+		action();
+	}
+}
+
+public static class ExtensionMethods
+{
+	public static UnityWebRequestAwaiter GetAwaiter(this UnityWebRequestAsyncOperation request)
+	{
+		return new UnityWebRequestAwaiter(request);
+	}
+}
+
+public class LoadTextureAsync : MonoBehaviour
+{
+	async void Load (Material material, string property, string filepath)
+	{
+		UnityWebRequest request = UnityWebRequestTexture.GetTexture(filepath);
+		await request.SendWebRequest();
+		material.SetTexture(property, ((DownloadHandlerTexture)request.downloadHandler).texture);
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+			Load(GetComponent<Renderer>().material, "_MainTex", System.IO.Path.Combine(Application.streamingAssetsPath, "plasma.png"));
+	}
+}
