9998e94e9b39d6d51d3f7fe20e6c08bc96edfd9c
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2022-08-25 13:08:11 +0200

    Load Scene Async Await

---
+++ LoadSceneAsyncAwait.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneAsyncAwait : MonoBehaviour
+{
+	async Task LoadSceneTask (string sceneName)
+	{
+		AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+		asyncOperation.allowSceneActivation = false;
+		while (asyncOperation.progress < 0.9f) { }
+		asyncOperation.allowSceneActivation = true;
+	}
+
+	async void LoadScene (string sceneName)
+	{
+		await LoadSceneTask (sceneName);
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			LoadScene ("Menu");
+		}
+	}
+}
